684d821126ba0960040481a99ad6414bd8c726c6
Matt Rizzo <mattrizzo21@gmail.com>
2020-07-12 14:06:49 -0400

    Corrected install paradigm for venv

--- installation/install.bat
+++ installation/install.bat
@@ -1,17 +1,20 @@
 set start_loc=%CD%
 cd ../
+set venv_dir_name=virtual_environment
 set project_root_dir=%CD%
-set venv_python_path=%project_root_dir%\virtual_environment\Scripts\
+set venv_root_path=%project_root_dir%\%venv_dir_name%
+set venv_scripts_path=%venv_root_path%\Scripts\
 set python_program=python.exe
-cd %venv_python_path%
+
+:: Create the venv
+python -m venv %venv_root_path%
+
+cd %venv_scripts_path%
 
 :: Just in case, update pip
 %python_program% -m pip install --upgrade pip
 
-:: Unistall the current versions to the venv
-%python_program% -m pip uninstall -r %start_loc%/requirements.txt -y
-
-:: Install the correct versions to the venv
+:: Install the modules (with their correct versions) to the venv
 %python_program% -m pip install -r %start_loc%/requirements.txt
 
 cd %start_loc%
