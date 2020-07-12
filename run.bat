f82349b9d7aca1a9074248aed44502fee5eb86c0
Matt Rizzo <mattrizzo21@gmail.com>
2020-07-12 17:34:48 -0400

    Run.bat now uses/checks for windwos venv

--- run.bat
+++ run.bat
@@ -1,1 +1,13 @@
-python src/__main__.py
+@echo off
+:: Get path to venv python
+SET path_to_windows_venv=%CD%\virtual_environment_windows\Scripts
+SET python_program=python.exe
+SET full_python_path=%path_to_windows_venv%\%python_program%
+
+:: Check that the the venv is installed
+if EXIST %full_python_path% ( 
+    %path_to_windows_venv%\%python_program% src\__main__.py
+) ELSE (
+    echo The Windows Virtual Environment is not setup.
+    echo Please run installation\install.bat and then rerun this script 
+)
