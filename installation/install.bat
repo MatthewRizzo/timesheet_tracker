4b24b76512fb1672f725a2f36c3c7e09164cee7e
Matt Rizzo <mattrizzo21@gmail.com>
2020-07-12 13:55:05 -0400

    Added venv install for windows

--- installation/install.bat
+++ installation/install.bat
@@ -1,5 +1,17 @@
-:: Unistall the current versions
-python -m pip uninstall -r requirements.txt -y
+set start_loc=%CD%
+cd ../
+set project_root_dir=%CD%
+set venv_python_path=%project_root_dir%\virtual_environment\Scripts\
+set python_program=python.exe
+cd %venv_python_path%
 
-:: 
-python -m pip install -r requirements.txt
+:: Just in case, update pip
+%python_program% -m pip install --upgrade pip
+
+:: Unistall the current versions to the venv
+%python_program% -m pip uninstall -r %start_loc%/requirements.txt -y
+
+:: Install the correct versions to the venv
+%python_program% -m pip install -r %start_loc%/requirements.txt
+
+cd %start_loc%
